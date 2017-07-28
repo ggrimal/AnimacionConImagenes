f6ac826418187322c6aa9bd56388e7a55e63b9fa
Brinkisi <brinkisi007@hotmail.com>
2017-07-28 17:36:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/brink/animacionconimagenes/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.brink.animacionconimagenes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imagen = (ImageView) findViewById(R.layout.img);
+
+        imagen.setBackgroundResource(R.drawable.pandita);
+    }
+}
